781db51af8ff9fcdd7143dae94a1aaa3d9dbc79a
MehrajShakil <54067524+MehrajShakil@users.noreply.github.com>
2020-06-28 18:09:26 +0600

    Add files via upload

---
+++ DP on Subarray/maximum sum of a  subarray using dp.cpp
@@ -0,0 +1,102 @@
+///   BISMILLAHIR RAHMANIR RAHEEM
+///   ALLAH IS WATCHING ME
+///   ALLAH save us from COVID-19.Amin.
+
+///   █▀█─█──█──█▀█─█─█
+///   █▄█─█──█──█▄█─█▄█
+///   █─█─█▄─█▄─█─█─█─█
+
+///   كُلُّ نَفْسٍ ذَآئِقَةُ الْمَوْت
+///   Every soul shall taste death.
+
+/// TODO::
+
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/detail/standard_policies.hpp>
+
+///  order_of_key return number of elements less than x.
+///  find_by_order return index.
+
+using namespace std;
+using namespace __gnu_pbds;
+
+
+#define MOHAMMAD  ios::sync_with_stdio(0);cin.tie(0);
+#define all(x)    (x).begin(), (x).end()
+#define AE        cout << fixed << setprecision(10);
+
+int dx8[] = {0, 0, 1, 1, 1, -1, -1, -1}; // 8-direction.......
+int dy8[] = {1,-1, 1, -1, 0, 0, -1, 1};
+int dx4[]= {1,-1,0,0}; // 4-direction...........
+int dy4[]= {0,0,1,-1};
+
+typedef tree< int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+typedef tree<pair<int, int>,null_type,less<pair<int, int>>,rb_tree_tag,tree_order_statistics_node_update> ordered_multiset;
+
+//debug
+template<typename F,typename S>ostream&operator<<(ostream&os,const pair<F,S>&p){return os<<"("<<p.first<<", "<<p.second<<")";}
+template<typename T>ostream&operator<<(ostream&os,const vector<T>&v){os<<"{";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"}";}
+template<typename T>ostream&operator<<(ostream&os,const set<T>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<",";os<<*it;}return os<<"]";}
+template<typename T>ostream&operator<<(ostream&os,const multiset<T>&v) {os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<*it;}return os<<"]";}
+template<typename F,typename S>ostream&operator<<(ostream&os,const map<F,S>&v){os<<"[";for(auto it=v.begin();it!=v.end();++it){if(it!=v.begin())os<<", ";os<<it->first<<" = "<<it->second;}return os<<"]";}
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+void faltu(){cerr << endl;}
+template<typename T>void faltu(T a[],int n){for(int i=0;i<n;++i)cerr<<a[i]<<' ';cerr<<endl;}
+template<typename T,typename...hello>void faltu(T arg,const hello&...rest){cerr<<arg<<' ';faltu(rest...);}
+//#else
+//#define dbg(args...)
+//***********************************************  The END **********************************************************************************************************************************
+using ll = long long;
+
+const ll N = 1e5;
+
+ll dp[N + 1] , n , a[N + 1] , mx = 0;
+
+ ll recur ( ll i , ll to ){
+
+   if ( i>n ) return 0;
+
+   if(dp[i]!=-1){
+               // dbg ( to , i , dp[i] );
+                return dp[i];
+   }
+   ll mx = 0;
+
+   mx =  max ( mx , a[i] + recur ( i + 1 , to ) );
+
+   dp[i] = mx;
+
+  // dbg ( to ,  mx , i );
+   return mx;
+
+ }
+
+int main()
+{
+
+   MOHAMMAD
+   cin >> n;
+
+   memset ( dp , -1 , sizeof dp );
+
+   for ( ll i = 1 ; i<=n ; ++i )cin >> a[i];
+
+   for ( ll i = 1 ; i<=n ; ++i ){
+        mx = max ( mx , recur ( i , i ) );
+   }
+
+   cout << mx << "\n";
+
+}
+
+/*
+
+
+
+
+
+  Alhamdulillah
+*/
+
